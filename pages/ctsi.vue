0d7b377f877f899d204836afd0c7afac0400527d
Minh Nguyen <minhnguyen@Minhs-MBP.vf.shawcable.net>
2021-11-06 14:29:48 -0700

    Add Cartesi ticker

---
+++ pages/ctsi.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+		<!-- TradingView Widget BEGIN -->
+		<div class="tradingview-widget-container">
+			<div class="tradingview-widget-container__widget"></div>
+			<script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-single-quote.js" async>
+			{
+				"symbol": "BINANCE:CTSIUSDT",
+				"width": 350,
+				"colorTheme": "dark",
+				"isTransparent": false,
+				"locale": "en"
+			}
+			</script>
+		</div>
+		<!-- TradingView Widget END -->
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
